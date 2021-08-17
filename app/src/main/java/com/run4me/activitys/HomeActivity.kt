ebe75c606ee80c5757d59ef958a95a8caee01fbb
vishnu <vishnudevxamarin.gmail.com>
2021-08-17 23:40:26 +0530

    Home page and Profile Page design Completed

--- app/src/main/java/com/run4me/activitys/HomeActivity.kt
+++ app/src/main/java/com/run4me/activitys/HomeActivity.kt
@@ -1,12 +1,21 @@
 package com.run4me.activitys
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.run4me.R
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var img_home_profile: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        img_home_profile=findViewById(R.id.img_home_profile)
+        img_home_profile.setOnClickListener {
+            var gotoProfilePage=Intent(this,ProfileActivity::class.java)
+            startActivity(gotoProfilePage)
+
+        }
     }
 }
---
+++ app/src/main/java/com/run4me/activitys/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.run4me.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.run4me.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
